6d2a61e41ffc94e2e0d2e66f5524f8943b963bc8
DanCiciarello <me@danieleciciarello.com>
2022-05-25 19:30:06 +0200

    Completed snack 3

--- Snack 3/main.js
+++ Snack 3/main.js
@@ -4,3 +4,31 @@ Il programma stampa la somma di tutti i numeri inseriti. */
 
 /* ------------------------------------------------------------- */
 
+// Recupero gli elementi dal dom
+let inputResultElement = document.querySelector("#inputResult");
+
+// Creo una variabile per la somma
+let numSum = 0;
+
+// Ciclo per richiedere i numeri all'utente
+let i = 0;
+while (i < 5) {
+
+    // Chiedo un numero all'utente
+    let inputNum = parseInt(prompt("Inserisci un numero"));
+
+    // Controllo che l'input sia un numero
+    if (isNaN(inputNum)) {
+        alert("Per favore, riprova inserendo un numero valido.");
+    } else {
+        // Sommo il numero inserito
+        numSum += inputNum;
+    
+        // Aggiungo una iterazione al conteggio
+        i++
+    }
+
+}
+
+// Stampo la somma finale dei numeri
+inputResultElement.innerHTML = `La somma dei numeri inseriti è ${numSum}.`
