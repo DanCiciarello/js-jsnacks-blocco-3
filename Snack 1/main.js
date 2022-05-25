723eb9fa518484dbbc0a981eae60b6211aad26ef
DanCiciarello <me@danieleciciarello.com>
2022-05-25 18:52:22 +0200

    Completed snack 1

--- Snack 1/main.js
+++ Snack 1/main.js
@@ -5,3 +5,22 @@ nell’array fino a quando la somma degli elementi è minore di 50. */
 
 /* ------------------------------------------------------------- */
 
+// Creo un array vuoto
+let numsArray = [];
+
+// Creo una variabile per sommare i numeri
+let numsSum = 0;
+
+// Ciclo di richiesta all'utente
+while (numsSum < 50) {
+
+    // Chiedo un numero all'utente
+    let numInput = parseInt(prompt("Inserisci un numero"));
+
+    // Aggiungo il numero all'array
+    numsArray.push(numInput);
+
+    // Aggiungo il numero alla somma
+    numsSum += numInput;
+
+};
