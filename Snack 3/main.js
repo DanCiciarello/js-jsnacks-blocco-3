98faa60091db83ddf440743dda30db74368ad058
DanCiciarello <me@danieleciciarello.com>
2022-05-25 18:42:38 +0200

    Start project

---
+++ Snack 3/main.js
@@ -0,0 +1,6 @@
+/* Richiesta Snack3
+Il software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
+
+/* ------------------------------------------------------------- */
+
